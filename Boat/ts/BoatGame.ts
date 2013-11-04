1115c935e525043c9b29f356420ec519b4574ad4
Alexandre <alex@euler.(none)>
2013-11-04 10:05:43 -0200

    New scenes: Winner / Loser

--- Boat/ts/BoatGame.ts
+++ Boat/ts/BoatGame.ts
@@ -2,6 +2,8 @@
 ///<reference path='Scene.ts'/>
 ///<reference path='Menu.ts'/>
 ///<reference path='Boat.ts'/>
+///<reference path='WinScene.ts'/>
+///<reference path='LoseScene.ts'/>
 
 class BoatGame {
     private game: Phaser.Game;
@@ -27,6 +29,8 @@ class BoatGame {
     public create(): void {
         this.game.state.add('Menu', new Menu(this.game), false);
         this.game.state.add('Boat', new Boat(this.game), false);
+        this.game.state.add('Win', new WinScene(this.game), false);
+        this.game.state.add('Lose', new LoseScene(this.game), false);
         this.game.state.start('Menu', true, true);
     }
 }
