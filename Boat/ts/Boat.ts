ad984afc2afbfc072f356bf0d1dba0306092819f
Vinícius <vkdaros@ime.usp.br>
2013-11-11 09:49:39 -0200

    Explosions disappear after the end of their animation.

--- Boat/ts/Boat.ts
+++ Boat/ts/Boat.ts
@@ -169,7 +169,7 @@ class Boat extends Scene {
         expl.animations.add('exploding',
                             [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],
                             20, false, true);
-        expl.animations.play('exploding', 20, false);
+        expl.animations.play('exploding', 20, false, true);
     }
 
     public setScene(sceneName: string) {
